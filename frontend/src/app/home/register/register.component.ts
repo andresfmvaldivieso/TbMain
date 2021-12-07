b0c422691e010a7b7004c885def61170ee26d6ed
andresfmvaldivieso <andresfmvaldivieso@gmail.com>
2021-12-07 11:04:46 -0500

    resolve error

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -41,7 +41,7 @@ export class RegisterComponent implements OnInit {
       this._userService.registerUser(this.registerData).subscribe({
         next:(v) => {
           localStorage.setItem('token', v.token);
-          this._router.navigate(['/saveTask']);
+          this._router.navigate(['/save-task']);
           this.message = "Successful user registration"
           this.openSnackBarSuccesFull();
         },
